328cd0c54f2ff891a2241dc207a84bd41e359067
Brian Shimkus <brianshimkus1@gmail.com>
2022-02-11 00:07:26 -0600

    Added routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 2000
 
 const app = express()
 
 app.get('/', (req, res) => {
 	res.status(200).json({ message: 'Hello' })
 })
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
