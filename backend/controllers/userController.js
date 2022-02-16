6b92fa07761e1591846d3f42a6ac210c23005655
Brian Shimkus <brianshimkus1@gmail.com>
2022-02-15 22:58:06 -0600

    Set up error and exception handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,21 @@
+// @desc: Register a new user
+// @route /api/users
+// @access: Public
 const registerUser = (req, res) => {
+	const { name, email, password } = req.body
+
+	// Validation
+	if (!name || !email || !password) {
+		res.status(400)
+		throw new Error('Please enter all fields')
+	}
+
 	res.send('Register Route')
 }
 
+// @desc: Login a user
+// @route /api/login
+// @access: Public
 const loginUser = (req, res) => {
 	res.send('Login Route')
 }
