8b12edac8b49b2542dbf5acb5912e59287dba5fc
Brian Shimkus <brianshimkus1@gmail.com>
2022-03-20 11:07:13 -0500

    Implemented useEffect for registering users

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
 import { useDispatch, useSelector } from 'react-redux'
-import { register } from '../features/auth/authSlice'
+import { register, reset } from '../features/auth/authSlice'
+import { useNavigate } from 'react-router-dom'
 
 export default function Register() {
 	const [formData, setFormData] = useState({
@@ -15,10 +16,24 @@ export default function Register() {
 	const { name, email, password, password2 } = formData
 
 	const dispatch = useDispatch()
+	const navigate = useNavigate()
 
-	const { user, isLoading, isSuccess, message } = useSelector(
+	const { user, isLoading, isError, isSuccess, message } = useSelector(
 		(state) => state.auth
 	)
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(message)
+		}
+
+		// Redirect when logged in
+		if (isSuccess || user) {
+			navigate('/')
+		}
+
+		dispatch(reset())
+	}, [isError, isSuccess, user, message, navigate, dispatch])
 
 	const onChange = (e) => {
 		setFormData((prevState) => ({
