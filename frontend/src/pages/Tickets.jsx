df2917ab96ff45b464f8aa78fb6b471b85096cec
Brian Shimkus <brianshimkus1@gmail.com>
2022-04-01 16:35:17 -0500

    Displaying tickets on ticket page

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getTickets, reset } from '../features/tickets/ticketSlice'
 import Spinner from '../components/Spinner'
+import { BackButton } from '../components/BackButton'
+import TicketItem from '../components/TicketItem'
 
 export default function Tickets() {
 	const { tickets, isLoading, isSuccess } = useSelector(
@@ -26,5 +28,21 @@ export default function Tickets() {
 		return <Spinner />
 	}
 
-	return <div>Tickets</div>
+	return (
+		<>
+			<BackButton url='/' />
+			<h1>Tickets</h1>
+			<div className='tickets'>
+				<div className='ticket-headings'>
+					<div>Date</div>
+					<div>Product</div>
+					<div>Status</div>
+					<div></div>
+				</div>
+				{tickets.map((ticket) => (
+					<TicketItem key={ticket._id} ticket={ticket} />
+				))}
+			</div>
+		</>
+	)
 }
