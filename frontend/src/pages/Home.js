155f288f991b98de1d6ff639dca74d8a138d2b09
Brian Shimkus <brianshimkus1@gmail.com>
2022-04-13 00:46:11 -0500

    Re-styled log in page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom'
 export default function Home() {
 	return (
 		<div className='container mx-auto px-4 text-center'>
-			<section className='mt-24 mb-12'>
-				<h1 className='text-3xl'>What do you need help with?</h1>
+			<section className='mt-12 lg:mt-24 mb-12'>
+				<h1 className='text-4xl'>What do you need help with?</h1>
 				<p className='text-lg mt-2 text-gray-400 italic'>
 					Please choose from an option below
 				</p>
 			</section>
 			<Link to='/new-ticket'>
-				<span className='bg-red-400 text-red-900 rounded py-2 px-3 hover:bg-red-300 uppercase transition ease-in-out duration-300 align-middle ml-4'>
+				<span className='bg-red-400 text-red-900 rounded p-4 hover:bg-red-300 uppercase transition ease-in-out duration-300 align-middle text-xl block lg:inline'>
 					Create New Ticket
 				</span>
 			</Link>
 
 			<Link to='/tickets'>
-				<span className='bg-gray-400 text-gray-900 rounded py-2 px-3 hover:bg-gray-300 uppercase transition ease-in-out duration-300 align-middle ml-4'>
+				<span className='bg-gray-400 text-gray-900 rounded p-4 hover:bg-gray-300 uppercase transition ease-in-out duration-300 align-middle ml-0 lg:ml-8 text-xl block lg:inline mt-6 lg:mt-0'>
 					View My Tickets
 				</span>
 			</Link>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -57,20 +57,20 @@ export default function Login() {
 	}
 
 	return (
-		<>
-			<section className='heading'>
-				<h1>
-					<FaSignInAlt /> Login
-				</h1>
-				<p>Please log in to get support</p>
+		<div className='container mx-auto px-4 text-center'>
+			<section className='mt-12 lg:mt-24 mb-12'>
+				<h1 className='text-4xl'>Login</h1>
+				<p className='text-lg mt-2 text-gray-400 italic'>
+					Please log in to get support
+				</p>
 			</section>
 
 			<section className='form'>
 				<form onSubmit={onSubmit}>
-					<div className='form-group'>
+					<div className='mb-6'>
 						<input
 							type='email'
-							className='form-control'
+							className='bg-slate-500 text-slate-900 rounded p-4 w-full xl:w-1/2 hover:bg-slate-400 transition ease-in-out duration-300 focus:outline-none focus:shadow-outline focus:bg-slate-400 placeholder-slate-300'
 							id='email'
 							name='email'
 							value={email}
@@ -82,7 +82,7 @@ export default function Login() {
 					<div className='form-group'>
 						<input
 							type='password'
-							className='form-control'
+							className='bg-slate-500 text-slate-900 rounded p-4 w-full xl:w-1/2 hover:bg-slate-400 transition ease-in-out duration-300 focus:outline-none focus:shadow-outline focus:bg-slate-400 placeholder-slate-300'
 							id='password'
 							name='password'
 							value={password}
@@ -91,11 +91,13 @@ export default function Login() {
 							required
 						/>
 					</div>
-					<div className='form-group'>
-						<button className='btn btn-block'>Submit</button>
+					<div className='mt-6'>
+						<button className='bg-red-400 text-xl rounded p-4 w-full xl:w-1/2 text-red-900 uppercase hover:bg-red-300 transition ease-in-out duration-300'>
+							Log In
+						</button>
 					</div>
 				</form>
 			</section>
-		</>
+		</div>
 	)
 }
