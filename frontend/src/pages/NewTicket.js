8b9e3f0f855b9b50682c9933bc595bb87ca08247
Brian Shimkus <brianshimkus1@gmail.com>
2022-04-14 18:59:32 -0500

    Re-styled new ticket page

--- frontend/src/pages/NewTicket.js
+++ frontend/src/pages/NewTicket.js
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { createTicket, reset } from '../features/tickets/ticketSlice'
 import Spinner from '../components/Spinner'
-import BackButton from '../components/BackButton'
 
 function NewTicket() {
 	const { user } = useSelector((state) => state.auth)
@@ -43,52 +42,61 @@ function NewTicket() {
 	}
 
 	return (
-		<>
-			<BackButton url='/' />
-			<section className='heading'>
-				<h1>Create New Ticket</h1>
-				<p>Please fill out the form below</p>
+		<div className='container mx-auto px-4 text-center'>
+			<section className='mt-12 lg:mt-24 mb-12'>
+				<h1 className='text-4xl'>Create New Ticket</h1>
+				<p className='text-lg mt-2 text-gray-400 italic'>
+					Please fill out the form below
+				</p>
 			</section>
 
 			<section className='form'>
-				<div className='form-group'>
-					<label htmlFor='name'>Customer Name</label>
-					<input type='text' className='form-control' value={name} disabled />
+				<div className='mb-6'>
+					<input
+						type='text'
+						className='bg-gray-400 text-slate-900 rounded p-4 w-full xl:w-1/2 transition ease-in-out duration-300 focus:outline-none focus:shadow-outline focus:bg-slate-400 placeholder-slate-300'
+						value={name}
+						disabled
+					/>
 				</div>
-				<div className='form-group'>
-					<label htmlFor='email'>Customer Email</label>
-					<input type='text' className='form-control' value={email} disabled />
+				<div className='mb-6'>
+					<input
+						type='text'
+						className='bg-gray-400 text-slate-900 rounded p-4 w-full xl:w-1/2 transition ease-in-out duration-300 focus:outline-none focus:shadow-outline focus:bg-slate-400 placeholder-slate-300'
+						value={email}
+						disabled
+					/>
 				</div>
 				<form onSubmit={onSubmit}>
-					<div className='form-group'>
-						<label htmlFor='product'>Product</label>
+					<div className='mb-6'>
 						<select
 							name='product'
 							id='product'
+							className='bg-slate-500 text-slate-900 rounded p-4 w-full xl:w-1/2 hover:bg-slate-400 transition ease-in-out duration-300 focus:outline-none focus:shadow-outline focus:bg-slate-400 placeholder-slate-300'
 							value={product}
 							onChange={(e) => setProduct(e.target.value)}>
+							<option class='placeholder'>Choose Product</option>
 							<option value='iPhone'>iPhone</option>
 							<option value='Macbook Pro'>Macbook Pro</option>
 							<option value='iMac'>iMac</option>
 							<option value='iPad'>iPad</option>
 						</select>
 					</div>
-					<div className='form-group'>
-						<label htmlFor='description'>Description of the issue</label>
+					<div className='mb-6'>
 						<textarea
 							name='description'
 							id='description'
-							className='form-control'
+							className='bg-slate-500 text-slate-900 rounded p-4 w-full xl:w-1/2 hover:bg-slate-400 transition ease-in-out duration-300 focus:outline-none focus:shadow-outline focus:bg-slate-400 placeholder-slate-300'
 							placeholder='Description'
 							value={description}
 							onChange={(e) => setDescription(e.target.value)}></textarea>
 					</div>
-					<div className='form-group'>
-						<button className='btn btn-block'>Submit</button>
-					</div>
+					<button className='bg-red-400 text-xl rounded p-4 w-full xl:w-1/2 text-red-900 uppercase hover:bg-red-300 transition ease-in-out duration-300'>
+						Create Ticket
+					</button>
 				</form>
 			</section>
-		</>
+		</div>
 	)
 }
 
